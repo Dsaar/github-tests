25fcc125bc02cba42995bfc3c36b64626c51378f
Dsaar <danieljoshuasaar@gmail.com>
2025-05-09 13:48:11 +0300

    add

--- fighting-game copy/src/classes.js
+++ fighting-game copy/src/classes.js
@@ -34,10 +34,12 @@ class Sprite {
 		if (this.framesElapsed % this.framesHold === 0) {
 			if (this.framesCurrent < this.framesMax - 1) {
 				this.framesCurrent++
+
 			} else {
 				this.framesCurrent = 0
 			}
 		}
+
 	}
 
 	update() {
@@ -65,7 +67,9 @@ class Fighter extends Sprite {
 				x: this.position.x,
 				y: this.position.y
 			},
+
 			offset: attackBox.offset,
+
 			width: attackBox.width,
 			height: attackBox.height
 		}
@@ -84,32 +88,42 @@ class Fighter extends Sprite {
 		}
 	}
 
+
+
 	update() {
 		this.draw()
 		if (!this.dead) this.animateFrames()
 
+		//attack boxes
 		this.attackBox.position.x = this.position.x + this.attackBox.offset.x
 		this.attackBox.position.y = this.position.y + this.attackBox.offset.y
+
+		//draw the attack boxes
+		//ctx.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
 
 		this.position.x += this.velocity.x
 		this.position.y += this.velocity.y
 
-		const groundLevel = canvas.height - 96
-		if (this.position.y + this.height + this.velocity.y >= groundLevel) {
-			this.velocity.y = 0
-			this.position.y = groundLevel - this.height
+		if (this.position.y + this.height + this.velocity.y >= canvas.height - 96) {
+			this.velocity.y = 0;
+			this.position.y = 330
 		} else {
 			this.velocity.y += gravity
-		}
-	}
 
+
+		}
+
+
+	}
 	attack() {
 		this.switchSprite('attack1')
 		this.isAttacking = true
+
 	}
 
 	takeHit() {
 		this.health -= 20
+
 		if (this.health <= 0) {
 			this.switchSprite('death')
 		} else this.switchSprite('takeHit')
@@ -117,11 +131,19 @@ class Fighter extends Sprite {
 
 	switchSprite(sprite) {
 		if (this.image === this.sprites.death.image) {
-			if (this.framesCurrent === this.sprites.death.framesMax - 1) this.dead = true
+			if (this.framesCurrent === this.sprites.death.framesMax - 1)
+				this.dead = true
 			return
 		}
-		if (this.image === this.sprites.attack1.image && this.framesCurrent < this.sprites.attack1.framesMax - 1) return
-		if (this.image === this.sprites.takeHit.image && this.framesCurrent < this.sprites.takeHit.framesMax - 1) return
+
+
+		//overwritting all other animations with the attack animation
+		if (this.image === this.sprites.attack1.image && this.framesCurrent < this.sprites.attack1.framesMax - 1)
+			return
+
+		//overwrite when fighter gets hit
+		if (this.image === this.sprites.takeHit.image && this.framesCurrent < this.sprites.takeHit.framesMax - 1)
+			return
 
 		switch (sprite) {
 			case 'idle':
@@ -130,49 +152,61 @@ class Fighter extends Sprite {
 					this.framesMax = this.sprites.idle.framesMax
 					this.framesCurrent = 0
 				}
-				break
+
+
+				break;
 			case 'run':
 				if (this.image !== this.sprites.run.image) {
 					this.image = this.sprites.run.image
 					this.framesMax = this.sprites.run.framesMax
 					this.framesCurrent = 0
+
 				}
-				break
+
+
+				break;
 			case 'jump':
 				if (this.image !== this.sprites.jump.image) {
 					this.image = this.sprites.jump.image
 					this.framesMax = this.sprites.jump.framesMax
 					this.framesCurrent = 0
+
 				}
-				break
+				break;
+
 			case 'fall':
 				if (this.image !== this.sprites.fall.image) {
 					this.image = this.sprites.fall.image
 					this.framesMax = this.sprites.fall.framesMax
 					this.framesCurrent = 0
+
 				}
-				break
+				break;
 			case 'attack1':
 				if (this.image !== this.sprites.attack1.image) {
 					this.image = this.sprites.attack1.image
 					this.framesMax = this.sprites.attack1.framesMax
 					this.framesCurrent = 0
+
 				}
-				break
+				break;
+
 			case 'takeHit':
 				if (this.image !== this.sprites.takeHit.image) {
 					this.image = this.sprites.takeHit.image
 					this.framesMax = this.sprites.takeHit.framesMax
 					this.framesCurrent = 0
+
 				}
-				break
+				break;
+
 			case 'death':
 				if (this.image !== this.sprites.death.image) {
 					this.image = this.sprites.death.image
 					this.framesMax = this.sprites.death.framesMax
 					this.framesCurrent = 0
 				}
-				break
+				break;
 		}
 	}
 }
